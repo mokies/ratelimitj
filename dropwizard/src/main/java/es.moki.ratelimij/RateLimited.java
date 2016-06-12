134767e4b22e719edb162696d6448d85a3b82566
Craig Baker <craigbbaker@gmail.com>
2016-06-12 15:37:06 +1000

    Initial dropward integration framework classes

---
+++ dropwizard/src/main/java/es.moki.ratelimij/RateLimited.java
@@ -0,0 +1,12 @@
+package es.moki.ratelimij;
+
+import javax.ws.rs.NameBinding;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@NameBinding
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface RateLimited { }
