aca236587a7a78a096e6618196b69441e985d23e
Craig Baker <craigbbaker@gmail.com>
2016-12-12 21:30:25 +1100

    Improvements to Key and introduced Key tests

--- ratelimitj-dropwizard/src/main/java/es/moki/ratelimij/dropwizard/filter/Key.java
+++ ratelimitj-dropwizard/src/main/java/es/moki/ratelimij/dropwizard/filter/Key.java
@@ -2,34 +2,81 @@ package es.moki.ratelimij.dropwizard.filter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.container.ResourceInfo;
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
-import static java.util.Objects.nonNull;
+import static java.util.Objects.isNull;
 
 public enum Key implements KeyProvider {
 
     DEFAULT {
+
         @Override
         public String create(final HttpServletRequest request,
                              final ResourceInfo resource) {
-            return "dwf:" + requestKey(request).orElse("-") + ":" + resourceKey(resource);
+            return "dw-ratelimit:" + requestKey(request) + ":" + resourceKey(resource);
+        }
+
+        private String requestKey(final HttpServletRequest request) {
+            return selectOptional(
+                    () -> userRequestKey(request),
+                    () -> xForwardedForRequestKey(request),
+                    () -> ipRequestKey(request))
+                    .orElse("NO_REQUEST_KEY");
         }
     };
 
-    private static final String HEADER = "X-Forwarded-For";
+    private static final String X_FORWARDED_FOR = "X-Forwarded-For";
 
     private static String resourceKey(final ResourceInfo resource) {
-        return resource.getResourceClass().getTypeName() + ":" + resource.getResourceMethod().getName();
+        return resource.getResourceClass().getTypeName() +
+                "#" + resource.getResourceMethod().getName();
     }
 
-    private static Optional<String> requestKey(final HttpServletRequest request) {
-        if (nonNull(request.getRemoteUser())) {
-            return Optional.of("duser:" + request.getRemoteUser());
-        } else if (nonNull(request.getHeader(HEADER))) {
-            return Optional.of("x-fwd-for:" + request.getHeader(HEADER));
-        } else if (nonNull(request.getRemoteAddr())) {
-            return Optional.of("ip:" + request.getRemoteAddr());
+//    private static Optional<String> requestKey(final HttpServletRequest request) {
+//        if (nonNull(request.getRemoteUser())) {
+//            return Optional.of("user#" + request.getRemoteUser());
+//        } else if (nonNull(request.getHeader(HEADER))) {
+//            return Optional.of("xfwd4#" + request.getHeader(HEADER));
+//        } else if (nonNull(request.getRemoteAddr())) {
+//            return Optional.of("ip#" + request.getRemoteAddr());
+//        }
+//        return Optional.empty();
+//    }
+
+    static Optional<String> userRequestKey(HttpServletRequest request) {
+        String remoteUser = request.getRemoteUser();
+        if (isNull(remoteUser)) {
+            return Optional.empty();
         }
-        return Optional.empty();
+        return Optional.of("user#" + remoteUser);
+    }
+
+    static Optional<String> xForwardedForRequestKey(HttpServletRequest request) {
+
+        String header = request.getHeader(X_FORWARDED_FOR);
+        if (isNull(header)) {
+            return Optional.empty();
+        }
+
+        Optional<String> originatingClientIp = Stream.of(header.split(",")).findFirst();
+        return originatingClientIp.map(ip -> "xfwd4#" + ip);
+    }
+
+    static Optional<String> ipRequestKey(HttpServletRequest request) {
+        String remoteAddr = request.getRemoteAddr();
+        if (isNull(remoteAddr)) {
+            return Optional.empty();
+        }
+        return Optional.of("ip#" + remoteAddr);
+    }
+
+    @SafeVarargs
+    static <T> Optional<T> selectOptional(Supplier<Optional<T>>... optionals) {
+        return Arrays.stream(optionals)
+                .reduce((s1, s2) -> () -> s1.get().map(Optional::of).orElseGet(s2))
+                .orElse(Optional::empty).get();
     }
 }
---
+++ ratelimitj-dropwizard/src/test/java/es/moki/ratelimij/dropwizard/filter/KeyTest.java
@@ -0,0 +1,78 @@
+package es.moki.ratelimij.dropwizard.filter;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.container.ResourceInfo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class KeyTest {
+
+    @Mock
+    HttpServletRequest request;
+
+    @Mock
+    ResourceInfo resource;
+
+    @Before
+    public void beforeEach() throws Exception {
+
+        doReturn(Object.class).when(resource).getResourceClass();
+        when(resource.getResourceMethod()).thenReturn(Object.class.getMethod("wait", null)) ;
+    }
+
+
+    @DisplayName("default key should start with 'dw-ratelimit' prefix")
+    @Test
+    public void shouldStartWithPrefix() {
+        String keyName = Key.DEFAULT.create(request, resource);
+
+        System.out.println(keyName);
+
+        assertThat(keyName).startsWith("dw-ratelimit:");
+    }
+
+    @DisplayName("default key should include Class and Method names in key")
+    @Test
+    public void shouldEndWithResourceInKey() {
+
+        String keyName = Key.DEFAULT.create(request, resource);
+
+        System.out.println(keyName);
+
+        assertThat(keyName).endsWith("java.lang.Object#wait");
+    }
+
+
+    @DisplayName("default key should include user id if available")
+    @Test
+    public void shouldIncludeUserId() {
+        when(request.getRemoteUser()).thenReturn("elliot");
+
+        String keyName = Key.DEFAULT.create(request, resource);
+
+        assertThat(keyName).contains("user#elliot");
+    }
+
+    @DisplayName("default key should include user id if available")
+    @Test
+    public void shouldIncludeXForwardedForIfUserNull() {
+//        when(request.getRemoteUser()).thenReturn("elliot");
+        when(request.getHeader("X-Forwarded-For")).thenReturn("203.0.113.7,  211.1.16.2");
+
+        String keyName = Key.DEFAULT.create(request, resource);
+
+        assertThat(keyName).contains("xfwd4#203.0.113.7");
+    }
+
+}
