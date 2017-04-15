a8c293d27f2daaa149a71b438dc38526dc50a5cf
Craig Baker <craigbbaker@gmail.com>
2017-04-15 17:42:26 +1000

    Prevent instantiation of RateLimitUtils

--- ratelimitj-core/src/main/java/es/moki/ratelimitj/core/RateLimitUtils.java
+++ ratelimitj-core/src/main/java/es/moki/ratelimitj/core/RateLimitUtils.java
@@ -5,6 +5,10 @@ import javax.annotation.Nullable;
 
 public class RateLimitUtils {
 
+    private RateLimitUtils() {
+        // util class
+    }
+
     public static <T> T coalesce(@Nullable T first, T second) {
         return first == null ? second : first;
     }
