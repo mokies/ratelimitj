34a61aad81725bf0e1fd0c0dd5822ede8b8f66dd
Craig Baker <craigbbaker@gmail.com>
2017-04-15 12:35:46 +1000

    Improved code coverage of LimitRuleJsonSerialiser

--- ratelimitj-redis/src/test/java/es/moki/ratelimitj/redis/LimitRuleJsonSerialiserTest.java
+++ ratelimitj-redis/src/test/java/es/moki/ratelimitj/redis/LimitRuleJsonSerialiserTest.java
@@ -10,18 +10,25 @@ import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@RunWith(JUnitPlatform.class)
 public class LimitRuleJsonSerialiserTest {
 
     private final LimitRuleJsonSerialiser serialiser = new LimitRuleJsonSerialiser();
 
     @Test
-    @DisplayName("should Encode limit rule in JSON array")
+    @DisplayName("should encode limit rule in JSON array")
     public void shouldEncode() {
 
         ImmutableList<LimitRule> rules = ImmutableList.of(LimitRule.of(10, TimeUnit.SECONDS, 10L), LimitRule.of(1, TimeUnit.MINUTES, 20L));
 
         assertThat(serialiser.encode(rules)).isEqualTo("[[10,10],[60,20]]");
+    }
 
+    @Test
+    @DisplayName("should encode limit rule with precision in JSON array")
+    public void shouldEncodeWithPrecisions() {
+
+        ImmutableList<LimitRule> rules = ImmutableList.of(LimitRule.of(10, TimeUnit.SECONDS, 10L).withPrecision(4), LimitRule.of(1, TimeUnit.MINUTES, 20L).withPrecision(8));
+
+        assertThat(serialiser.encode(rules)).isEqualTo("[[10,10,4],[60,20,8]]");
     }
 }
