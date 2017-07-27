c6e5cd231ebad81775b2fbf8db695f5fee0ad650
Craig Baker <craigbbaker@gmail.com>
2017-07-27 21:00:57 +1000

    Deprecating AsyncRequestRateLimiter

--- ratelimitj-core/src/main/java/es/moki/ratelimitj/core/limiter/request/AsyncRequestRateLimiter.java
+++ ratelimitj-core/src/main/java/es/moki/ratelimitj/core/limiter/request/AsyncRequestRateLimiter.java
@@ -6,11 +6,15 @@ import java.util.concurrent.CompletionStage;
 /**
  * An asynchronous rate limiter interface supporting Java 8's {@link java.util.concurrent.CompletionStage}.
  */
+@Deprecated
 public interface AsyncRequestRateLimiter {
 
+    @Deprecated
     CompletionStage<Boolean> overLimitAsync(String key);
 
+    @Deprecated
     CompletionStage<Boolean> overLimitAsync(String key, int weight);
 
+    @Deprecated
     CompletionStage<Boolean> resetLimitAsync(String key);
 }
