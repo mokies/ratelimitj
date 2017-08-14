de325348d9bdbe65f65c47c870c346caea8c1529
Craig Baker <craigbbaker@gmail.com>
2017-08-14 21:34:26 +1000

    Remove the dependancy on Guava from the redis module

--- ratelimitj-redis/src/main/java/es/moki/ratelimitj/redis/request/RedisScriptLoader.java
+++ ratelimitj-redis/src/main/java/es/moki/ratelimitj/redis/request/RedisScriptLoader.java
@@ -1,12 +1,14 @@
 package es.moki.ratelimitj.redis.request;
 
 
-import com.google.common.base.Charsets;
-import com.google.common.io.Resources;
 import io.lettuce.core.api.StatefulRedisConnection;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 import static java.util.Objects.requireNonNull;
 
@@ -57,9 +59,15 @@ public class RedisScriptLoader {
     }
 
     private String readScriptFile() throws IOException {
-        // TODO remove guava depedency - java file loading is a mess!
-        URL url = Resources.getResource(scriptUri);
-        return Resources.toString(url, Charsets.UTF_8);
+        URL url = RedisScriptLoader.class.getClassLoader().getResource(scriptUri);
+
+        if (url == null) {
+            throw new IllegalArgumentException("script '" + scriptUri + "' not found");
+        }
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
     }
 
 }
