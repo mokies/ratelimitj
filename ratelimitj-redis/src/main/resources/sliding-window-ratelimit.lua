c0094dd60d839b361dbc49ba8cc8fc67a7e75b66
ctoomey <ctoomey@livongo.com>
2017-08-16 15:17:18 -0700

    Don't update redis when weight = 0

--- ratelimitj-redis/src/main/resources/sliding-window-ratelimit.lua
+++ ratelimitj-redis/src/main/resources/sliding-window-ratelimit.lua
@@ -63,13 +63,15 @@ for i, limit in ipairs(limits) do
 end
 
 -- there is enough resources, update the counts IFF needed
-for i, limit in ipairs(limits) do
-    local saved = saved_keys[i]
-    for j, key in ipairs(KEYS) do
-         -- update the current timestamp, count, and bucket count
-         redis.call('HSET', key, saved.ts_key, saved.trim_before)
-         redis.call('HINCRBY', key, saved.count_key, weight)
-         redis.call('HINCRBY', key, saved.count_key .. saved.block_id, weight)
+if weight ~= 0 then
+    for i, limit in ipairs(limits) do
+        local saved = saved_keys[i]
+        for j, key in ipairs(KEYS) do
+            -- update the current timestamp, count, and bucket count
+            redis.call('HSET', key, saved.ts_key, saved.trim_before)
+            redis.call('HINCRBY', key, saved.count_key, weight)
+            redis.call('HINCRBY', key, saved.count_key .. saved.block_id, weight)
+        end
     end
 end
 
