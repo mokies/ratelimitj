b71da2bbdb00430f930801c52f3e1280fa87347e
Craig Baker <craigbbaker@gmail.com>
2017-12-23 09:17:40 +1100

    Enhanced Redis Script loader tests

--- ratelimitj-redis/src/test/java/es/moki/ratelimitj/redis/request/RedisScriptLoaderTest.java
+++ ratelimitj-redis/src/test/java/es/moki/ratelimitj/redis/request/RedisScriptLoaderTest.java
@@ -2,21 +2,25 @@ package es.moki.ratelimitj.redis.request;
 
 
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.lettuce.core.ScriptOutputType.VALUE;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class RedisScriptLoaderTest {
 
     private static RedisClient client;
+    private static StatefulRedisConnection<String, String> connection;
 
     @BeforeAll
     static void beforeAll() {
         client = RedisClient.create("redis://localhost");
+        connection = client.connect();
     }
 
     @AfterAll
@@ -27,7 +31,7 @@ class RedisScriptLoaderTest {
     @Test
     @DisplayName("should load rate limit lua script into Redis")
     void shouldLoadScript() {
-        RedisScriptLoader scriptLoader = new RedisScriptLoader(client.connect(), "sliding-window-ratelimit.lua");
+        RedisScriptLoader scriptLoader = new RedisScriptLoader(connection, "hello-world.lua");
 
         assertThat(scriptLoader.scriptSha()).isNotEmpty();
     }
@@ -35,7 +39,7 @@ class RedisScriptLoaderTest {
     @Test
     @DisplayName("should eagerly load rate limit lua script into Redis")
     void shouldEagerlyLoadScript() {
-        RedisScriptLoader scriptLoader = new RedisScriptLoader(client.connect(), "sliding-window-ratelimit.lua", true);
+        RedisScriptLoader scriptLoader = new RedisScriptLoader(connection, "hello-world.lua", true);
 
         assertThat(scriptLoader.scriptSha()).isNotEmpty();
     }
@@ -45,7 +49,18 @@ class RedisScriptLoaderTest {
     void shouldFailedIfScriptNotFound() {
 
         Throwable exception = assertThrows(IllegalArgumentException.class,
-                () -> new RedisScriptLoader(client.connect(), "not-found-script.lua", true));
+                () -> new RedisScriptLoader(connection, "not-found-script.lua", true));
         assertThat(exception.getMessage()).contains("not found");
+    }
+
+    @Test
+    @DisplayName("should fail if script not found")
+    void shouldExecuteScript() {
+
+        RedisScriptLoader scriptLoader = new RedisScriptLoader(connection, "hello-world.lua", true);
+        String sha = scriptLoader.scriptSha();
+
+        Object result = connection.sync().evalsha(sha, VALUE);
+        assertThat((String) result).isEqualTo("hello world");
     }
 }
