6c1fd3eb25a729d25d3d1c1a649bf368507a9a84
abhijit.kumar <abhijit.kumar@PP-C02YQC4YLVCG.local>
2020-07-11 13:20:05 +0530

    aerospike config changes

--- ratelimitj-aerospike/src/test/java/es/moki/aerospike/extensions/AerospikeClientFactory.java
+++ ratelimitj-aerospike/src/test/java/es/moki/aerospike/extensions/AerospikeClientFactory.java
@@ -18,26 +18,26 @@ public class AerospikeClientFactory {
     readPolicy.maxRetries = 1;
     readPolicy.readModeAP = ReadModeAP.ONE;
     readPolicy.replica = Replica.MASTER_PROLES;
-    readPolicy.sleepBetweenRetries = 10;
-    readPolicy.totalTimeout = 100;
+    readPolicy.sleepBetweenRetries = 5;
+    readPolicy.totalTimeout = 1000;
     readPolicy.sendKey = true;
 
     WritePolicy writePolicy = new WritePolicy();
     writePolicy.maxRetries = 2;
     writePolicy.readModeAP = ReadModeAP.ALL;
     writePolicy.replica = Replica.MASTER_PROLES;
-    writePolicy.sleepBetweenRetries = 10;
+    writePolicy.sleepBetweenRetries = 5;
     writePolicy.commitLevel = CommitLevel.COMMIT_ALL;
     writePolicy.totalTimeout = 10000;
     writePolicy.sendKey = true;
 
     ClientPolicy clientPolicy = new ClientPolicy();
-    clientPolicy.maxConnsPerNode = 2;
+    clientPolicy.maxConnsPerNode = 1;
     clientPolicy.readPolicyDefault = readPolicy;
     clientPolicy.writePolicyDefault = writePolicy;
     clientPolicy.failIfNotConnected = true;
     clientPolicy.timeout = 30000;
-    clientPolicy.threadPool = Executors.newFixedThreadPool(5);
+    clientPolicy.threadPool = Executors.newFixedThreadPool(1);
 
     AerospikeClient aerospikeClient = new AerospikeClient(clientPolicy,
         new Host("127.0.0.1", 3000));
