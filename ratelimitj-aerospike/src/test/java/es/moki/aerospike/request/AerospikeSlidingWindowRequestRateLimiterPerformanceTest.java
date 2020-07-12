d5e57ecc839250b48f6bccca1328019da94a0d7e
abhijit.kumar <abhijit.kumar@PP-C02YQC4YLVCG.local>
2020-07-12 13:58:19 +0530

    correcting aerospike client race condition

--- ratelimitj-aerospike/src/test/java/es/moki/aerospike/request/AerospikeSlidingWindowRequestRateLimiterPerformanceTest.java
+++ ratelimitj-aerospike/src/test/java/es/moki/aerospike/request/AerospikeSlidingWindowRequestRateLimiterPerformanceTest.java
@@ -1,24 +1,34 @@
 package es.moki.aerospike.request;
 
-import es.moki.aerospike.extensions.AerospikeConnectionSetup;
+import com.aerospike.client.AerospikeClient;
+import es.moki.aerospike.extensions.AerospikeClientFactory;
+import es.moki.ratelimitj.aerospike.request.AerospikeContext;
 import es.moki.ratelimitj.aerospike.request.AerospikeSlidingWindowRateLimiter;
 import es.moki.ratelimitj.core.limiter.request.RequestLimitRule;
 import es.moki.ratelimitj.core.limiter.request.RequestRateLimiter;
 import es.moki.ratelimitj.core.time.TimeSupplier;
 import es.moki.ratelimitj.test.limiter.request.AbstractSyncRequestRateLimiterPerformanceTest;
 import java.util.Set;
-import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 
-public class AerospikeSlidingWindowRequestRateLimiterPerformanceTest extends
-    AbstractSyncRequestRateLimiterPerformanceTest {
+public class AerospikeSlidingWindowRequestRateLimiterPerformanceTest extends AbstractSyncRequestRateLimiterPerformanceTest {
 
-  @RegisterExtension
-  static AerospikeConnectionSetup aerospikeConnectionSetup = new AerospikeConnectionSetup();
+  private static AerospikeContext aerospikeContext;
+
+  @AfterAll
+  static void afterAll() {
+    aerospikeContext.aerospikeClient.close();
+  }
+
+  @BeforeAll
+  static void beforeAll() {
+    AerospikeClient aerospikeClient = AerospikeClientFactory.getAerospikeClient();
+    aerospikeContext = new AerospikeContext(aerospikeClient,"test","test");
+  }
 
   @Override
-  protected RequestRateLimiter getRateLimiter(
-      Set<RequestLimitRule> rules, TimeSupplier timeSupplier) {
-    return new AerospikeSlidingWindowRateLimiter(aerospikeConnectionSetup.getAerospikeContext(),
-        rules, timeSupplier);
+  protected RequestRateLimiter getRateLimiter(Set<RequestLimitRule> rules, TimeSupplier timeSupplier) {
+    return new AerospikeSlidingWindowRateLimiter(aerospikeContext, rules, timeSupplier);
   }
 }
